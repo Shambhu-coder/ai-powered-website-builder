a7fd9c40ba9deb297104748f92e1565e7114a77d
Shambanagouda Kallannanavar <smartshambhuk31@gmail.com>
2026-06-18 15:20:18 +0530

    auth log changed

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -26,7 +26,7 @@ const isAuth = async (req, res, next) => {
         }
 
         req.user = user
-        console.log("Authenticated user:", user);
+        console.log("Authenticated user updated:", user);
         next()
     } catch (error) {
         return res.status(500).json({ message: "auth error: " + error.message })
